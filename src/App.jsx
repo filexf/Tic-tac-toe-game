42026c10f51df70cb62a2e1c7324b8bce17b5d08
Félix Orain <felix.orain@gmail.com>
2025-03-23 02:02:23 +0100

    Changed the styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,31 @@ import GameBoard from "./components/GameBoard";
 import { useState } from "react";
 
 function App() {
-  const [ activePlayer, setActivePlayer ] = useState("X")
+  const [activePlayer, setActivePlayer] = useState("X");
 
   function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X")
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
   }
-  
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       LOGGING
     </main>
